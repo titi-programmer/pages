a6e1d03cea5339348edc2132eb2059f0cc84dddf
Tiago Fein <tgfein@gmail.com>
2024-09-29 18:48:17 -0300

    cpt

--- docs/js/orientate.js
+++ docs/js/orientate.js
@@ -2,6 +2,6 @@ const pageable = new Pageable("main", {
 	pips: false,
 	onFinish: function(data) {
 		// orientate horizontally when index is odd number
-		this.orientate(data.index % 2 == 0 ? "vertical" : "horizontal");
+		this.orientate(data.index % 2 == 0 ? "horizontal" : "horizontal");
 	},
 });
